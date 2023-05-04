0c055576d11fbbaf076a90e655a5616a18fb9aba
KEI025 <kei.bash0731@gmail.com>
2023-05-05 00:39:26 +0900

    お菓子画像追加と画面の表示実装

--- Magic Sweet Shop/Main.cpp
+++ Magic Sweet Shop/Main.cpp
@@ -16,6 +16,17 @@ void Main()
 	TextureAsset::Register(U"Ghost-0", U"img/ghost-0.png");
 	TextureAsset::Register(U"Ghost-1", U"img/ghost-1.png");
 
+	TextureAsset::Register(U"ice-0", U"img/ice-0.png");
+	TextureAsset::Register(U"ice-1", U"img/ice-1.png");
+	TextureAsset::Register(U"ice-2", U"img/ice-2.png");
+	TextureAsset::Register(U"ice-3", U"img/ice-3.png");
+	TextureAsset::Register(U"ice-4", U"img/ice-4.png");
+
+	TextureAsset::Register(U"donut-0", U"img/donut-0.png");
+	TextureAsset::Register(U"donut-1", U"img/donut-1.png");
+	TextureAsset::Register(U"donut-2", U"img/donut-2.png");
+	TextureAsset::Register(U"donut-3", U"img/donut-3.png");
+
 	FontAsset::Register(U"dot35",35, U"example/font/DotGothic16/DotGothic16-Regular.ttf", FontStyle::Bitmap);
 	FontAsset::Register(U"dot25",25, U"example/font/DotGothic16/DotGothic16-Regular.ttf",FontStyle::Bitmap);
 
--- Magic Sweet Shop/Menu.cpp
+++ Magic Sweet Shop/Menu.cpp
@@ -23,18 +23,42 @@ void Menu::draw() const {
 
 
 	//かんたん
+	const double ice_noise = Periodic::Sine0_1(2.5s) * 10;
 	const Vec2 esay_pos = { kWindowSize.x / 6, kWindowSize.y / 4.7 };
 
 	RoundRect{ 20,kWindowSize.x / 8,kCardSize, 10 }.drawFrame(1.5,Palette::Black);
 	FontAsset(U"dot25")(U"- EASY -").drawAt(esay_pos, Palette::Black);
 
+	FontAsset(U"dot25")(U"Time: 3 min").drawBase(esay_pos.movedBy(-110, 100), Palette::Black);
 	FontAsset(U"dot25")(U"Hi Score:").drawBase(esay_pos.movedBy(-110,60), Palette::Black);
 
+	TextureAsset(U"ice-0").scaled(0.07).drawAt(esay_pos.movedBy(-90,150 + ice_noise));
+	TextureAsset(U"ice-1").scaled(0.07).drawAt(esay_pos.movedBy(-45, 150 + ice_noise));
+	TextureAsset(U"ice-2").scaled(0.07).drawAt(esay_pos.movedBy(0,150 + ice_noise));
+	TextureAsset(U"ice-3").scaled(0.07).drawAt(esay_pos.movedBy(45, 150 + ice_noise));
+	TextureAsset(U"ice-4").scaled(0.07).drawAt(esay_pos.movedBy(90, 150 + ice_noise));
+
+
 	//ふつう
+	const Vec2 normal_pos = { kWindowSize.x / 2, kWindowSize.y / 4.7 };
+
 	RoundRect{ 20 * 2 + kCardSize.x,kWindowSize.x / 8,kCardSize, 10 }.drawFrame(1.5, Palette::Black);
 	FontAsset(U"dot25")(U"- NORMAI -").drawAt(kWindowSize.x / 2, kWindowSize.y / 4.7, Palette::Black);
 
+	FontAsset(U"dot25")(U"Hi Score:").drawBase(normal_pos.movedBy(-110, 60), Palette::Black);
+	FontAsset(U"dot25")(U"Time: 3 min").drawBase(normal_pos.movedBy(-110, 100), Palette::Black);
+
+	TextureAsset(U"donut-0").scaled(0.07).drawAt(normal_pos.movedBy(-90, 150 + ice_noise));
+	TextureAsset(U"donut-1").scaled(0.07).drawAt(normal_pos.movedBy(-45, 150 + ice_noise));
+	TextureAsset(U"donut-2").scaled(0.07).drawAt(normal_pos.movedBy(0, 150 + ice_noise));
+	TextureAsset(U"donut-3").scaled(0.07).drawAt(normal_pos.movedBy(45, 150 + ice_noise));
+
 	//むずかしい
+	const Vec2 hard_pos = { kWindowSize.x - (kCardSize.x / 2) - 20, kWindowSize.y / 4.7 };
+
 	RoundRect{ 20 * 3 + kCardSize.x * 2,kWindowSize.x / 8,kCardSize, 10 }.drawFrame(1.5, Palette::Black);
 	FontAsset(U"dot25")(U"- HARD -").drawAt(kWindowSize.x / 2 + kWindowSize.x / 3, kWindowSize.y / 4.7, Palette::Black);
+
+	FontAsset(U"dot25")(U"Hi Score:").drawBase(hard_pos.movedBy(-110, 60), Palette::Black);
+	FontAsset(U"dot25")(U"Time: 3 min").drawBase(hard_pos.movedBy(-110, 100), Palette::Black);
 }
